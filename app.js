fc89fac465beabf7fb642932c3d220959665a662
Mohian Dipta <moianmostofa@gmail.com>
2021-05-20 13:56:51 +0600

    reading environment variable

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
+require('dotenv/config')
+
+const api = process.env.API_URL
 
 
 // api will be in http://localhost:3000/api/v1/product
-app.get('/', (req, res) => {
+app.get(api + '/products', (req, res) => {
     res.send('Hello world')
 })
 
 
 app.listen(3000, () => {
+    console.log(api)
     console.log('Server is running  at http://localhost:3000')
 })
