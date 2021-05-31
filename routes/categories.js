ea649dc01d98eb1b8853ef7c970d7b34eff2b5a8
Mohian Dipta <mohianmostofa@gmail.com>
2021-05-31 13:19:37 +0600

    categories POST created, working perfectly

--- routes/categories.js
+++ routes/categories.js
@@ -2,11 +2,28 @@ const express = require('express')
 const { Category } = require('../model/category')
 const router = express.Router();
 
-//get
+
+
+//api will be in http://localhost:3000/api/v1/categories
+//get 
 router.get(`/`, async (req, res) => {
     const categoryList = await Category.find({})
     res.send(categoryList)
 })
 
+
+//post data
+router.post('/', async (req, res) => {
+    let category = new Category({
+        name: req.body.name,
+        icon: req.body.icon,
+        color: req.body.color
+    })
+    category = await category.save();
+    if (!category) {
+        console.log('the category can not be created')
+    }
+    res.send(category);
+})
 
 module.exports = router;
