a5bb93c9b90625f8b55c54f7104febfc80e8f08c
Mohian Dipta <mohianmostofa@gmail.com>
2021-06-10 14:12:33 +0600

    using regular expression instead of regular url path

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -3,6 +3,7 @@ const expressJwt = require('express-jwt')
 //prtecting api and authencticate using JWT middleware
 function authJwt() {
     const secret = process.env.secret
+    const api = process.env.API_URL
     return expressJwt({
         secret,
         algorithms: ['HS256']
@@ -10,9 +11,12 @@ function authJwt() {
         // excluding REST api routes from authentication
         .unless({
             path: [
-                { url: '/api/v1/products', methods: ['GET', 'OPTIONS'] },
-                '/api/v1/users/login',
-                '/api/v1/users/register'
+                {
+                    url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'],
+                    url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS']
+                },
+                `${api}/users/login`,
+                `${api}/users/register`,
             ]
         })
 }
