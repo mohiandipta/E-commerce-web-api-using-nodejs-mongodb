06b4038e63ae17124d0e521041eb94774c6f7cb3
Mohian Dipta <mohianmostofa@gmail.com>
2021-06-14 14:08:19 +0600

    add new order posting error

--- routes/orders.js
+++ routes/orders.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const { Order } = require('../model/order')
+const { Order } = require('../model/order');
+const { OrderItem } = require('../model/OrderItem');
 const { User } = require('../model/user')
 const router = express.Router();
 
@@ -20,6 +21,38 @@ router.get('/:id', async (req, res) => {
         return res.status(500).json({ message: 'the order with the given ID was not found' })
     }
     res.status(200).send(order)
+})
+
+
+//post order
+router.post('/', async (req, res) => {
+    const orderItemsIds = req.body.orderItems.map(async orderItem => {
+        newOrderItem = new OrderItem({
+            quantity: orderItem.quantity,
+            product: orderItem.product
+        })
+
+        newOrderItem = await newOrderItem.save()
+
+        return newOrderItem._id
+    })
+    let order = new Order({
+        orderItem: orderItemsIds,
+        shippingAddress1: req.body.shippingAddress1,
+        shippingAddress2: req.body.shippingAddress2,
+        city: req.body.city,
+        zip: req.body.city,
+        country: req.body.country,
+        phone: req.body.phone,
+        status: req.body.status,
+        totalPrice: req.body.totalPrice,
+        user: req.body.user
+    })
+    order = await order.save()
+    if (!order) {
+        return res.status(400).send('the order cannot be created')
+    }
+    res.send(order)
 })
 
 
